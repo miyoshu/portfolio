65b0d40154be8cdc9b31b50ae8e5780719c942b7
miyoshu <miyoshu1127@gmail.com>
2024-08-25 23:23:59 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+<meta charset="UTF-8">
+<link rel = "stylesheet" href="index.css">
+<title>ホーム画面</title>
+</head>
+
+<body>
+
+	<table class="table1">
+		<tr>
+			<td class="td1">
+				<a href="">
+					<img src="img/20.png" alt="ホーム" class="HOME">
+				</a>
+			</td>
+
+			<td class="td1">
+				<a href="">
+					<img src="img/19.png" alt="カート" class="CART">
+				</a>
+			</td>
+
+			<td class="td1">
+				<a href="login.jsp">
+					<img src="img/21.png" alt="メニュー" class="MENU">
+				</a>
+			</td>
+
+		</tr>
+	</table>
+
+	<div class="name">
+		ブランド名
+	</div>
+
+
+	<img src="img/7.jpg" class="TOP">
+
+	<div class="news">
+		NEWS
+	</div>
+
+	<!-- ニュース内容 -->
+
+	<div class="newitem">
+		NEW ITEM
+	</div>
+
+	<img src="img/4.JPG" class="itemimg">
+
+	Blanche Neige
+
+
+
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+    
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Login画面</title>
+</head>
+<body>
+	<div id="header">
+		<div id="pr">
+		</div>
+	</div>
+	<div id="main">
+		<div id="top">
+			<p>Login</p>
+		</div>
+		<div>
+			<h3>商品を購入する際にはログインをお願いします。</h3>
+			<form action="login_confirm.php" method="post">
+                <p>メールアドレス：<input type="text" name="mail"  required></p>
+                <p>パスワード：<input type="password" name="password"  required></p>
+                <p><input type="submit" name="login" value="ログイン"></p>
+            </form>
+			<br/>
+			<div id="text-link">
+				<p>新規ユーザー登録は
+					<a href=>こちら</a>
+				</p>
+				<p>Homeへ戻る場合は
+					<a href=>こちら</a>
+				</p>
+			</div>
+		</div>
+	</div>
+	<div>
+		<div id="pr">
+		</div>
+	</div>
+
+
+</body>
+</html>
